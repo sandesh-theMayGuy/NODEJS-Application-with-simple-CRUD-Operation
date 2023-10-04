7eaba708ccace2bd8add34c0d1c85cfa0da2d863
Sandesh <khatiwadasandesh01@gmail.com>
2023-10-04 19:22:21 +0545

    Update validateToken.js

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,7 +2,7 @@ export default function validateToken(req,res,next){
     if(req.headers.token===process.env.TOKEN){
             next();
     }else{
-        res.status(503).json({
+        res.status(403).json({
             status:"Failed",
             message:"Invalid Token"
         })
